717274b47bb7f7509427732d648135ae3eb9af04
Mateus Bessa <mateusbessa@gmail.com>
2021-10-31 12:24:17 -0300

    commit

--- code.py
+++ code.py
@@ -2,25 +2,27 @@ import streamlit as st
 import pandas as pd
 import joblib as jb
 import numpy as np
+from streamlit.type_util import Key
+import SessionState as ss
 
 model = jb.load('model_portabilidade.pkl')
+session = ss.get(run_id=0)
 
 st.title('Análise de provável evasão de clientes')
 
 with st.form(key="data_client"):
     name = st.text_input(label='Nome do Cliente')
-    credit_score = st.number_input('Score',min_value=0,max_value=2000)
+    credit_score = st.number_input(label='Score',min_value=0,max_value=2000)
     tenure = st.number_input(label='Tempo de relacionamento com o banco',min_value=0,max_value=100)
     balance = st.number_input(label='Saldo em conta')
-    num_of_products = st.number_input('Produtos adquiridos',min_value=0,max_value=99)
+    num_of_products = st.number_input(label='Produtos adquiridos',min_value=0,max_value=99)
     has_crcard = st.checkbox(label='Tem cartão de crédito')
     is_active_member = st.checkbox(label='Membro ativo')
-    estimated_salary = st.number_input('Salário',min_value=0,max_value=200000)
-    input_buttom = st.form_submit_button("Analisar")
+    estimated_salary = st.number_input(label='Salário',min_value=0,max_value=200000)
+    input_buttom = st.form_submit_button('Analisar')
     test = np.array([[credit_score,tenure, balance,num_of_products,has_crcard,is_active_member,estimated_salary]])
     classify = model.predict(test)
     result = model.predict_proba(test)
- 
 
 if input_buttom:
     if classify == 1:
